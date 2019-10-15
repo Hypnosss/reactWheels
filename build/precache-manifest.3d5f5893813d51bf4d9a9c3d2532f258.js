35edf12741074bce7426e4541bb60e99825cda5b
Hypnosss <398906857@qq.com>
2019-10-15 09:10:44 +0800

    rebuild after upload

---
+++ build/precache-manifest.3d5f5893813d51bf4d9a9c3d2532f258.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "604250678f2c1f18c613079d08917d49",
+    "url": "./index.html"
+  },
+  {
+    "revision": "084cef9423c3cded27dc",
+    "url": "./static/css/main.b137362f.chunk.css"
+  },
+  {
+    "revision": "f8beea0c48450f0d9827",
+    "url": "./static/js/2.652c77be.chunk.js"
+  },
+  {
+    "revision": "084cef9423c3cded27dc",
+    "url": "./static/js/main.18679dc0.chunk.js"
+  },
+  {
+    "revision": "5269c111e02da5c7ec9b",
+    "url": "./static/js/runtime-main.034b6476.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.e79a6eaf28752436f88bced18c9665ea.js"
+  "./precache-manifest.3d5f5893813d51bf4d9a9c3d2532f258.js"
 );
 
 self.addEventListener('message', (event) => {
